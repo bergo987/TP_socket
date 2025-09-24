e5e7a66b4d41d1ce4cc1f7267043eaa2fa64b2a2
BERTHOD Hugo <Hugo.Berthod@bordeaux-inp.fr>
2025-09-24 12:26:02 +0200

    Socket, bind, listen accept sont opperationel, il faut à present gérer les interactions une fois que la connection est établie.

--- TCP/snippet_serveurTcp.c
+++ TCP/snippet_serveurTcp.c
@@ -10,21 +10,23 @@
 #define BUFFSIZE 256
 
   /* Taille de la file d'attente  initialisé par listen*/
-#define BACKLOG 1 
-#define MAXHOSTNAME 10
+#define BACKLOG 10
+#define MAXHOSTNAME 64
 
 /* Nombres de requiêtes traitées par le programme */
 #define MAXREQ 10
 
-int main(argc,argv)
+int main(int argc,char **argv)
 {
   int s ,t ; // descripteur de socket 
+  int count_req; 
+  int i ; 
 
+  void quiest();
   struct sockaddr_in sa;	/* Structure Internet sockaddr_in */
   struct hostent *hptr; 	/* Infos sur le serveur */
   struct servent *sp ; /* Structure service internet */
   int port;        		/* Numero de port du serveur */
-
   int newsd;			/* Id de la socket entrante */
   struct sockaddr_in newsa;	/* sockaddr_in de la connection entrante */
   int newsalength;
@@ -41,24 +43,48 @@ int main(argc,argv)
   sa.sin_addr.s_addr = INADDR_ANY;
 
   char localhost[MAXHOSTNAME]; 
+  /* Recuperation du numéro de port serveur dans la structure sa */
+  sa.sin_port = htons(port) ; 
 
-  sp = getservbyname("key", "tcp") ; 
+  /* création de la socket */
+  s = socket(AF_INET , SOCK_STREAM,0); 
+  if (s <0 ){
+    perror("Serveur : problème lors de la création socket"); 
+    exit (1); 
+  }
 
-  if (( sp == NULL )){
-    printf("Pas de servie QUI sur ce système"); 
+  /* Lien socket avec adresse IP et Port */
+  if(bind(s,&sa, sizeof(sa))<0){
+    printf("Serveur : problème bind \n"); 
     exit(1); 
   }
-  /* Recuperation du numéro de port serveur dans la structure sa */
-  sa.sin_port = htons(sp->s_port) ; 
+  listen(s,BACKLOG); 
+  printf("Serveur TCP en écoute\n");
 
-  /* Récuperation nom de la machine hote, ici localhost */
-  gethostname (localhost, MAXHOSTNAME);
-  /* Remplit la structure adresse sa */
-  hptr = gethostbyname(localhost) ; 
-  bcopy(hptr->h_addr,(char*)&sa.sin_addr,hptr->h_length) ; 
-  sa.sin_family = AF_INET ; 
-  /* Allocation d'un descripteur de socket */
-  s = socket(AF_INET , SOCK_STREAM,0); 
-  if (s <0 )
+  for (count_req = 0 ; count_req <= MAXREQ ; count_req++){
+    /* Attend une requête sur la primitive accept */
+    i = sizeof(newsa); 
+    newsalength = sizeof(newsa) ;
+    newsd = accept(s, &newsa, &newsalength);
+    if(newsd < 0){
+      printf("Erreur sur accept\n");
+      exit(1);
+    }
+
+    printf("Connection N° %d sur le port %d...\n", i, ntohs(newsa.sin_port));
+    printf("Requête n° %d\n", count_req); 
+    sleep(10);
+    /* Traitement de la requête*/
+    quiest();
+    close(t); 
+  }
+  /* Fermeture de la socket reserve  */
+  close(s); 
+  printf("fin de service pour %s\n", localhost);
+  exit(0); 
+}
+
+void quiest(){ /* Traitement de la requête */
+  printf("Je suis trop fort\n");
 
 }
