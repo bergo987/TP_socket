25cc81f57bc90312706b480bea8716e01c93b7f9
BERTHOD Hugo <Hugo.Berthod@bordeaux-inp.fr>
2025-10-01 12:28:18 +0200

    Fin de l'optimisation de la communication
    Il reste le traitement de la requête a faire

--- TCP/snippet_serveurTcp.c
+++ TCP/snippet_serveurTcp.c
@@ -10,6 +10,8 @@
 #define MAX_REQUEST 10
 #define BUFF_SIZE 256
 
+
+
 int main(int argc,char *argv[])
 {
     int sd;
@@ -65,18 +67,21 @@ int main(int argc,char *argv[])
         }
         /* Compteur nombre de connexion */
         i++;
-        /* nom du client */
+        /* nom du client */ 
         printf("Connection No %d sur le port %d...\nDebut de la discussion\n", i, ntohs(newsa.sin_port));
-       
-        /*Début de la discussion avec le client N°i*/
-        /*On écoute la connexion entrante*/
-        n = read(newsd, buff, BUFF_SIZE);
-        write(1,buff, n);// on affiche la réponse du client
-        printf("Réponse n°%d : %s\n",i,buff);
-        /*char msg[BUFF_SIZE] ;
-        scanf("%[^\n]",msg);*/
-        write(newsd, buff, n);
-        printf("fin de l'envoi au client\n ");
+		/*Début de la discussion avec le client N°i*/
+		/*On écoute la connexion entrante*/
+		n = read(newsd, buff, BUFF_SIZE);
+		//write(1,buff, n);// on affiche la réponse du client
+		printf("Réponse n°%d : %s\n",i,buff);
+		/*char msg[BUFF_SIZE] ;
+		scanf("%[^\n]",msg);*/
+
+		/*Traitement de la requête*/
+
+		write(newsd, buff, n);//envoie de la reponse
+		//sleep(0.1);
+		printf("fin de l'envoi au client\n ");
         close(newsd);
     }
     /* Fermeture du serveur. Never reached */
