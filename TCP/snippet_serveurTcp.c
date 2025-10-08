773eab10169f8b01981c4094995fc8ca09d895ef
BERTHOD Hugo <Hugo.Berthod@bordeaux-inp.fr>
2025-10-08 12:27:43 +0200

    Début de l'implémentation de la calculatrice

--- TCP/snippet_serveurTcp.c
+++ TCP/snippet_serveurTcp.c
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <netdb.h>
 #include <string.h>
 #include <strings.h>
@@ -10,6 +11,81 @@
 #define MAX_REQUEST 10
 #define BUFF_SIZE 256
 
+char* calculatrice (char cal[]){
+    int nb = 0 ;
+    char s_a[BUFF_SIZE];
+    char s_b[BUFF_SIZE];
+    float a; 
+    float b;
+    char op = '0';
+    int m = 0 ; // longueur de a
+    int j = -1 ; // indice de l'op
+    int k = 0 ; // longueur de b
+    int etat = 0 ; 
+    float res = 0; 
+    char* ret = malloc(BUFF_SIZE*sizeof(char)); 
+    for(int i = 0 ; i < BUFF_SIZE&& cal[i]!='\0'; i ++ ){
+        if ((cal[i]== '+'|| cal[i]== '-' || cal[i]== '*'||cal[i]== '/')&& op =='0'){
+            op = cal[i]; 
+            j = i ; 
+        }
+        else {
+            if (j == -1 ){
+                m +=1 ; 
+                s_a[i] = cal[i];
+            }
+            else if (op!='0');{
+                s_b[i-j-1] = cal[i];
+                k += 1;
+            }
+        }
+    }
+    if((m != 0 && k != 0)&& (op != '0')){
+        nb=3;
+    }
+    a = atof(s_a); 
+    b = atof(s_b);
+    if (nb == 3){
+        printf("op = %c\t j = %d\n",op,j);
+        printf("a = %f \t b = %f\n m = %d \t k = %d\n",a,b,m,k);
+
+        if (op == '*'){
+            res = a*b ;
+            etat = 1;
+            printf("res* = %f\n",res);  
+        }
+        if (op == '+'){
+            res = a+b;
+            etat = 1;
+            printf("res+ = %f \n ",res);  
+        }
+        if (op == '-'){
+            res = a - b;
+            etat = 1; 
+            printf("res- = %f \n ",res);  
+        }
+        if (op == '/'){
+            res = a/b;
+            etat = 1;
+            printf("res/ = %f \n",res);
+        }
+        if (etat == 0 ){
+            printf("on ne connait pas l'op\n");
+            sprintf("on ne connait pas l'op\n", ret); 
+        }
+        else{
+            sprintf("%.02f",ret,res); 
+
+            printf("ret = %s \t strlen = %d \n ", ret,strlen(ret));
+        }
+    }
+    
+    else {
+    printf("Erreur dans la requette\n"); 
+    ret = "Erreur dans la requette";
+    }
+    return ret ;
+}
 
 
 int main(int argc,char *argv[])
@@ -25,6 +101,7 @@ int main(int argc,char *argv[])
     int i;
     char buff[BUFF_SIZE];
     int n ; 
+    char* res_calc = (char*)malloc(BUFF_SIZE*sizeof(char));
     /* verification du nombre d'arguments de la ligne de commande */
     if (argc != 2) {
         printf("pingserveurTCP. Erreur d'arguments\n");
@@ -54,11 +131,10 @@ int main(int argc,char *argv[])
     }
     /* Initialisation de la queue d'ecoute des requetes*/
     listen(sd, MAX_REQUEST);
-    printf("Serveur pingTCP en ecoute...\n");
+    printf("Serveur TCP en ecoute...\n");
     i = 0;
 
     while(1) {
-        //fflush(stdout);// on clean le flux entrant (inutile si pas de scanf)
         /* newsalength contient la taille de la structure sa attendue */
         newsalength = sizeof(newsa) ;
         if((newsd = accept(sd, (struct sockaddr *)&newsa, &newsalength)) < 0 ) {
@@ -72,19 +148,20 @@ int main(int argc,char *argv[])
 		/*Début de la discussion avec le client N°i*/
 		/*On écoute la connexion entrante*/
 		n = read(newsd, buff, BUFF_SIZE);
-		//write(1,buff, n);// on affiche la réponse du client
-		printf("Réponse n°%d : %s\n",i,buff);
-		/*char msg[BUFF_SIZE] ;
-		scanf("%[^\n]",msg);*/
-
+		printf("Réponse n°%d : %s\n n= %d\n",i,buff,n);
+        
 		/*Traitement de la requête*/
 
-		write(newsd, buff, n);//envoie de la reponse
-		//sleep(0.1);
+        res_calc = calculatrice(buff);
+
+        printf("resultat calculette = %s\n",res_calc);
+		write(newsd, res_calc, strlen(res_calc));//envoi de la reponse
 		printf("fin de l'envoi au client\n ");
         close(newsd);
+        
     }
     /* Fermeture du serveur. Never reached */
+    free(res_calc); 
     close(sd);
     printf("Fin du serveur. Bye...\n");
     exit(0);
